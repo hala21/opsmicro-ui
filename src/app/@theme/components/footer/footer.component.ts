14e093fe9d9dfcdba091b70bab71e9ea43b6061e
李志勇 <lizhiyong@iscs.com.cn>
2019-08-01 13:33:59 +0800

    init

---
+++ src/app/@theme/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-footer',
+  styleUrls: ['./footer.component.scss'],
+  template: `
+    <span class="created-by">Update By leo.li base on akveo.com <b>
+      <a href="https://akveo.com" target="_blank">akveo</a></b> 2019.08</span>
+    <div class="socials">
+      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+    </div>
+  `,
+})
+export class FooterComponent {
+}
